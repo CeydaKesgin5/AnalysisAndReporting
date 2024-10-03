d9e1a30acfc2a1aeff7b00983b4b0d5b742c3a6a
Ceydakesgin5 <ceydakesgin778@gmail.com>
2024-10-03 14:19:44 +0300

    first commit

---
+++ src/test/java/com/example/Analysis_and_Reporting/AnalysisAndReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Analysis_and_Reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysisAndReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
