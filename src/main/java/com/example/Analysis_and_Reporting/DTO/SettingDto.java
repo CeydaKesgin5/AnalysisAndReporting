4a2e8339e8eb6fbb76d8c73d24b46a212afd9b10
yarentag <yarentag23@gmail.com>
2024-10-31 11:29:03 +0300

    SettingDTO olusturuldu ve güncellendi

---
+++ src/main/java/com/example/Analysis_and_Reporting/DTO/SettingDto.java
@@ -0,0 +1,10 @@
+package com.example.Analysis_and_Reporting.DTO;
+
+import lombok.Data;
+
+@Data
+public class SettingDto{
+    private Long id;
+    private String settingKey;
+    private String settingValue;
+}
