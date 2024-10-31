b66d0dd3fa5b1703ce9a33bc6d87b4a77f5ad252
yarentag <yarentag23@gmail.com>
2024-10-31 11:22:22 +0300

    Musteri entity olusturuldu ve güncellendi

---
+++ src/main/java/com/example/Analysis_and_Reporting/Entity/Musteri.java
@@ -0,0 +1,50 @@
+package com.example.Analysis_and_Reporting.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "musteri", schema = "kargo_sistemi")
+public class Musteri {
+    public Musteri() {
+    }
+
+    // Parametreli yapıcı
+    public Musteri(String ad, String soyad, String telefon, String adres) {
+        this.adi = ad;
+        this.soyadi = soyad;
+        this.telefon = telefon;
+        this.adres = adres;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, length = 100)
+    private String adi;
+
+    @Column(nullable = false, length = 100)
+    private String soyadi;
+
+    @Column(nullable = false, length = 20)
+    private String telefon;
+
+    @Column(nullable = false)
+    private String adres;
+
+    @Column(unique = true, length = 100)
+    private String email;
+
+    private String createdBy;
+    private LocalDateTime createdAt;
+    private String updatedBy;
+    private LocalDateTime updatedAt;
+    private String deletedBy;
+    private LocalDateTime deletedAt;
+    private Integer version;
+}
+
