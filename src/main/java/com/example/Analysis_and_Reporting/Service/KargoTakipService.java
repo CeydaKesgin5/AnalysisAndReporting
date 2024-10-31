8f897bd336bbdf32c065d436001c0abcaeed9bf0
fyarenozcelik <yrn.ozclk71@gmail.com>
2024-10-31 12:38:50 +0300

    KargoTakip ve Calisan güncellendi

---
+++ src/main/java/com/example/Analysis_and_Reporting/Service/KargoTakipService.java
@@ -0,0 +1,25 @@
+
+
+import com.example.Analysis_and_Reporting.Entity.KargoTakip;
+import com.example.Analysis_and_Reporting.Repository.KargoTakipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class KargoTakipService {
+    private final KargoTakipRepository kargoTakipRepository;
+
+    @Autowired
+    public KargoTakipService(KargoTakipRepository kargoTakipRepository) {
+        this.kargoTakipRepository = kargoTakipRepository;
+    }
+
+    public KargoTakip saveKargoTakip(KargoTakip kargoTakip) {
+        return kargoTakipRepository.save(kargoTakip);
+    }
+
+    public List<KargoTakip> getKargoTakipByKargoId(Long kargoId) {
+        return kargoTakipRepository.findByKargo_KargoId(kargoId);
+    }
+}
